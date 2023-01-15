8cdc0be898dd3a845dce1841ac303b804d7058b0
Oscar Mumm <oscarmumm@gmail.com>
2023-01-15 14:38:06 -0300

    tarjeta producto solo falta animacion de hover

--- src/Components/TarjetaProducto/index.jsx
+++ src/Components/TarjetaProducto/index.jsx
@@ -24,16 +24,16 @@ function TarjetaProducto(){
     <div className='tarjetaProducto'>
       {json.map((producto) => (
         <article className='tarjetaProducto_contenedor'>
-          <section className='tarjetaProducto_fila'>
+          <section className='tarjetaProducto_fila tarjetaProducto_fila-foto'>
             <img src={producto.thumbnail} alt="miniatura" />
           </section>
           <div className='tarjetaProducto_lineaHorizontal'></div>
           <section className='tarjetaProducto_fila tarjetaProducto_fila-column tarjetaProducto_fila-izquierda tarjetaProducto_fila-padding'>
-          <span className='tarjetaProducto_textoSecundario tarjetaProducto_textoSecundario-tachado tarjetaProducto_textoSecundario-s14'>${Math.round((producto.price)/((100 - producto.discountPercentage)/100))}</span>
-          <p className='tarjetaProducto_precioNuevo'>${producto.price}<span className='tarjetaProducto_descuento'>{Math.round(producto.discountPercentage)}% OFF</span></p>
-          <span className='tarjetaProducto_descuento'>6x ${Math.round(producto.price / 6)} sin interes</span>
-          <span className='tarjetaProducto_descuento tarjetaProducto_descuento-negrita'> Envio gratis 💡 FULL</span>
-          <span class="tarjetaProducto_textoSecundario">{producto.title}</span>
+            <span className='tarjetaProducto_textoSecundario tarjetaProducto_textoSecundario-tachado tarjetaProducto_textoSecundario-s14'>${Math.round((producto.price)/((100 - producto.discountPercentage)/100))}</span>
+            <p className='tarjetaProducto_precioNuevo'>${producto.price}<span className='tarjetaProducto_descuento'>  {Math.round(producto.discountPercentage)}% OFF</span></p>
+            <span className='tarjetaProducto_descuento'>6x ${Math.round(producto.price / 6)} sin interes</span>
+            <span className='tarjetaProducto_descuento tarjetaProducto_descuento-negrita'>Envio gratis ⚡<i className='full'>FULL</i></span>
+            <span class="tarjetaProducto_textoSecundario">{producto.title}</span>
           </section>
         </article>
       ))}
